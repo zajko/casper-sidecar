235cdae05aacad0cf8932c3bb56954fb2c4c8632
zajko <ja.zajko@gmail.com>
2025-03-12 21:29:24 +0100

    Aligning sidecar with recent changes in the node (#427)
    * Aligning sidecar with recent changes in the node
    
    * Preparing rc cut
    
    * Updating dependencies
    
    * Disabling artifacts cache

--- types/src/legacy_sse_data/fixtures.rs
+++ types/src/legacy_sse_data/fixtures.rs
@@ -392,9 +392,7 @@ const RAW_TRANSACTION_ACCEPTED: &str = r#"
           ]
         },
         "entry_point": "AddBid",
-        "scheduling": {
-          "FutureEra": 195120
-        },
+        "scheduling": "Standard",
         "target": "Native"
     }
   },
@@ -845,6 +843,8 @@ const RAW_DEPLOY_PROCESSED: &str = r#"{
                 "initiator": {
                     "PublicKey": "0203d98e2ec694b981cebd7cf35ac531d8717e86dc35912f2536b8807ad550621147"
                 },
+                "current_price": 1,
+                "refund": "100",
                 "error_message": "Error message 18290057561582514745",
                 "limit": "11209375253254652626",
                 "consumed": "10059559442643035623",
